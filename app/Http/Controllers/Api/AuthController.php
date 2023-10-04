e08462eb4d3be4e53681225196455df1bba8adb2
frexxx-7 <borzenkokiril9@gmail.com>
2023-10-04 16:24:06 +0300

    edit AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -15,35 +15,35 @@ class AuthController extends Controller
     public function signup(SignupRequest $request)
     {
         $data = $request->validated();
-        /** @var \app\Models\User $user */
+        /** @var \App\Models\User $user */
         $user = User::create([
-            'login' => $data['login'],
+            'name' => $data['name'],
+            'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
 
         $token = $user->createToken('main')->plainTextToken;
-
-        return response(compact($user, $token));
+        return response(compact('user', 'token'));
     }
-    public function singin(LoginRequest $request)
+
+    public function login(LoginRequest $request)
     {
         $credentials = $request->validated();
-
         if (!Auth::attempt($credentials)) {
             return response([
-                'message' => 'Provided email address or password in incorrect'
-            ]);
+                'message' => 'Provided email or password is incorrect'
+            ], 422);
         }
-        /** @var User $user */
 
+        /** @var \App\Models\User $user */
         $user = Auth::user();
         $token = $user->createToken('main')->plainTextToken;
-        return response(compact($user, $token));
+        return response(compact('user', 'token'));
     }
 
     public function logout(Request $request)
     {
-        /** @var User $user */
+        /** @var \App\Models\User $user */
         $user = $request->user();
         $user->currentAccessToken()->delete();
         return response('', 204);

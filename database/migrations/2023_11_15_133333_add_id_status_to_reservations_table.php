128a7f171b4c069e6818ce486ace2ee5722c94a6
frexxx-7 <borzenkokiril9@gmail.com>
2023-11-16 20:59:44 +0300

    edit mirgation

--- database/migrations/2023_11_15_133333_add_id_status_to_reservations_table.php
+++ database/migrations/2023_11_15_133333_add_id_status_to_reservations_table.php
@@ -4,26 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::table('reservations', function (Blueprint $table) {
-            $table->bigInteger('idStatus')->unsigned()->default(null);
-            $table ->foreign('idStatus')->references('id')->on('status_reservations');
-        });
-    }
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::table('reservations', function (Blueprint $table) {
+      $table->bigInteger('idStatus')->unsigned()->default(null);
+      $table->foreign('idStatus')->references('id')->on('status_reservations');
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('reservations', function (Blueprint $table) {
-            $table->dropColumn('idStatus');
-        });
-    }
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::table('reservations', function (Blueprint $table) {
+      $table->dropForeign('reservations_idstatus_foreign');
+      $table->dropColumn('idStatus');
+    });
+  }
 };
